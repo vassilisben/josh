9282b2878c7b042229a9061ea0a02f38a0fe1fee
burcuucetin <bc2714@columbia.edu>
2022-03-05 19:11:53 -0500

    added ast

---
+++ ast.ml
@@ -0,0 +1,80 @@
+
+type bop = Add | Sub | Mul | Div | Mod | Equal | Neq | Less | Leq | Greater | Geq | And | Or 
+type uop = Not
+
+type typ = Int | Bool | Float | Char | String | List of typ | Void |
+		RecordType of Id | FunkType of Id * typ * opt list
+
+type opt = Opt of typ * Id 
+
+type expr =
+  | IntLit of int
+  | BoolLit of bool
+  | FloatLit of float
+  | CharLit of char
+  | StrLit of string
+  | Id of string
+  | Binop of expr * bop * expr
+  | Unop of uop * expr
+  | Assign of string * expr
+  | ListLit of expr list
+  | ListAccess of expr list * expr
+
+type Funk = {
+  id: Id;
+  opts_list: opt list;
+  body: stmt list;
+  return_type: typ;
+}
+
+type stmt =
+  | Block of stmt list
+  | Expr of expr
+  | If of expr * stmt * stmt
+  | While of expr * stmt
+
+type bind = typ * string
+
+type program = {
+  locals: bind list;
+  body: stmt list;
+}
+
+
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | And -> "&&"
+  | Or -> "||"
+
+let rec string_of_expr = function
+    Literal(l) -> string_of_int l
+  | BoolLit(true) -> "true"
+  | BoolLit(false) -> "false"
+  | Id(s) -> s
+  | Binop(e1, o, e2) ->
+    string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+    "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
+                      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_typ = function
+    Int -> "int"
+  | Bool -> "bool"
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_program fdecl =
+  "\n\nParsed program: \n\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "\n"
